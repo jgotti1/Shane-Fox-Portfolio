b68d2161e30d531dc231b5b788dc49fa84fa5db4
“Foxshane997” <Foxshane997@gmail.com>
2024-06-26 21:58:02 -0700

    changed some typography

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import '../Hero.css';
-import heroImage from '../HeroBackground.jpg'; // Update with your image path
+import heroImage from '../HeroBackground.jpg';
 
 const Hero = () => {
   return (
     <div className="hero" style={{ backgroundImage: `url(${heroImage})` }}>
       <div className="hero-content">
-        <h1>Welcome to My Website</h1>
-        <p>Explore my work and projects</p>
+        <h1>Welcome to My Website!</h1>
+        <p>Explore my work and projects!</p>
       </div>
     </div>
   );

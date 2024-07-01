8130f159a72ab8b0003172fe5245fd7b1fe83217
“Foxshane997” <Foxshane997@gmail.com>
2024-06-30 23:45:23 -0700

    basically done with the contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,26 @@
 import React from "react";
+import "../Contact.css";
 
 function Contact() {
-    return (
-        <div>
-            <h1>
-                Contact STUB
-            </h1>
-        </div>
-    )
+  return (
+    <div className="container">
+      <h1>Contact Me</h1>
+      <div className="contact-info">
+        <p>
+          <a href="mailto:your-email@example.com">Email me!</a>
+        </p>
+        <p>
+          <a
+            href="https://www.linkedin.com/in/shane-fox-19ba382a9/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn Profile
+          </a>
+        </p>
+      </div>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;

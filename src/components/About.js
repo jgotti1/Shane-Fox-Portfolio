f96db254194bc94aaab9d60321f3352e772ce539
“Foxshane997” <Foxshane997@gmail.com>
2024-06-30 22:46:46 -0700

    made the about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,28 @@
 import React from "react";
+import "../About.css";
 
 function About() {
   return (
-    <div>
+    <div className="container">
       <h1>About Me</h1>
-      <p>My name is Shane Fox I Budding Web Developer from Las Vegas Nevada.</p>
+      <p>
+        My name is Shane Fox. I am a budding Web Developer from Las Vegas,
+        Nevada.
+      </p>
+      <p>
+        I am currently pursuing a Software Development Course at the University
+        of Nevada, Las Vegas. I have successfully completed modules in HTML,
+        CSS, UI/UX design, JavaScript, backend development, and React/Redux.
+        Throughout my coursework, I have built a strong foundation in both
+        frontend and backend technologies, enabling me to create dynamic,
+        responsive, and user-friendly web applications. I am passionate about
+        leveraging my skills to develop innovative solutions and continuously
+        learning new technologies to stay current in the ever-evolving field of
+        web development.
+      </p>
+      <p></p>
     </div>
   );
 }
 
 export default About;
-// WIP

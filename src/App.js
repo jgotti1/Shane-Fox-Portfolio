ac4f84c1b678720b32891adb55b540ce372986c1
jgotti1 <jgotti1@optonline.net>
2024-07-03 21:41:25 -0400

    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
@@ -11,7 +12,7 @@ import Contact from "./components/Contact";
 function App() {
   return (
     <div>
-    <Router>
+    <HashRouter>
       <div className="App">
         <NavBar />
         <Routes>
@@ -22,7 +23,7 @@ function App() {
         </Routes>
         <Footer />
       </div>
-    </Router>
+    </HashRouter>
     </div>
   );
 }
